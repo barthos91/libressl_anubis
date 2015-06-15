c45176cc889cef38677aea355d5f15f1b05016f4
barthos91 <tibtib38@hotmail.com>
2015-06-15 15:41:16 +0200

    remove trace

--- crypto/evp/e_anubis.c
+++ crypto/evp/e_anubis.c
@@ -174,27 +174,17 @@ anubis_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out, const unsigned char *in,
     size_t len)
 {
     EVP_ANUBIS_KEY *dat = (EVP_ANUBIS_KEY *)ctx->cipher_data;
-    
-printf("anubis_cipher_128: start : len %d\n",len);
-int i;
-//~ printf("in: "); for(i = 0; i < 16; i++) printf("%02x ",in[i]); printf("\n");
 
     if (dat->cbc){
-		//~ printf("cdc\n");
         (*dat->cbc)(in, out, len, &dat->ks, ctx->iv,ctx->encrypt);
 	}
-	else if (ctx->encrypt){
-		//~ printf("ctx_encrypt\n");
-		CRYPTO_cbc128_encrypt(in, out, len, &dat->ks, ctx->iv,dat->block);
-	}
-	else{
-		//~ printf("ctx_decrypt\n");
-		CRYPTO_cbc128_decrypt(in, out, len, &dat->ks, ctx->iv,dat->block);
-	}
-                
+	//~ else if (ctx->encrypt){
+		//~ CRYPTO_cbc128_encrypt(in, out, len, &dat->ks, ctx->iv,dat->block);
+	//~ }
+	//~ else{
+		//~ CRYPTO_cbc128_decrypt(in, out, len, &dat->ks, ctx->iv,dat->block);
+	//~ }
 
-printf("out: "); for(i = 0; i < 16; i++) printf("%02x ",out[i]); printf("\n");
-//~ printf("anubis_cipher_128: end funstion\n");
 
     return 1;
 }
