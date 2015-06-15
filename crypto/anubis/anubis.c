7a8da2d5ec460cc661d025ace6d3079fbffa59d9
barthos91 <tibtib38@hotmail.com>
2015-06-15 11:09:30 +0200

    nativ version libre ssl

---
+++ crypto/anubis/anubis.c
@@ -0,0 +1,8 @@
+#include <openssl/anubis.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void cypher()
+{
+printf("je chiffre 2\n");
+}
---
+++ tests/test_perso.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <openssl/anubis.h>
+int main(int argc, char** argv)
+{
+printf("test OK\n");
+cypher(); 
+return 0;
+}
